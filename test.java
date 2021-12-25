40a4758fee8252d47d3000a431367f7d811ec0fc
safa alipoor <safaalipoor1431@gmail.com>
2021-12-25 15:56:25 +0330

    edit test

--- test.java
+++ test.java
@@ -3,6 +3,6 @@ import java.util.scanner;
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("java programming in amazing");
+        System.out.println("java");
     }
 }
